7a9927b47f0dbe42e2d62b34cefc81aecee9564e
seonjiwon <tommys915@gmail.com>
2026-03-20 15:08:23 +0900

    init

--- src/main/java/dev/fisa/domain/config/RestClientConfig.java
+++ src/main/java/dev/fisa/domain/config/RestClientConfig.java
@@ -1,5 +1,6 @@
 package dev.fisa.domain.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -7,4 +8,13 @@ import org.springframework.web.client.RestClient;
 @Configuration
 public class RestClientConfig {
 
+    @Value("${gateway.base-url}")
+    private String gatewayUrl;
+
+    @Bean
+    public RestClient gatewayRestClient() {
+        return RestClient.builder()
+                         .baseUrl(gatewayUrl)
+                         .build();
+    }
 }
